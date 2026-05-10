db5b0390142b0e2908677d0829b264fb389522d3
William Jackson <wjackson07281985@gmail.com>
2026-05-10 06:25:27 -0400

    fix(ui): reset scroll on main navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
-import { NavLink, Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import { NavLink, Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/Home";
 import PantryPage from "./pages/Pantry";
@@ -6,9 +7,24 @@ import RecipeBrowserPage from "./pages/RecipeBrowser";
 import RecommendationsPage from "./pages/Search";
 import RecipeDetailPage from "./pages/RecipeDetail";
 
+function ScrollToTop() {
+  const { hash, pathname } = useLocation();
+
+  useEffect(() => {
+    if (hash) {
+      return;
+    }
+
+    window.scrollTo({ top: 0, left: 0 });
+  }, [hash, pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <div className="app-shell">
+      <ScrollToTop />
       <nav className="top-nav">
         <NavLink to="/" end>
           Dinner Tonight
